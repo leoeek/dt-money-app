c23dbc385c6c3114050ec1aab03077986066440f
leoprocopio <leonardolp@gmail.com>
2025-08-17 13:40:26 -0300

    feat: register and login

---
+++ src/context/auth.context.tsx
@@ -0,0 +1,64 @@
+import { FormLoginParams } from "@/screens/Login/LoginForm";
+import { FormRegisterParams } from "@/screens/Register/RegisterForm";
+import {
+  createContext,
+  FC,
+  PropsWithChildren,
+  useContext,
+  useState,
+} from "react";
+import * as authService from "@/shared/services/dt-money/auth.service";
+import { IUser } from "@/shared/interfaces/user-interface";
+
+type AuthContextType = {
+  user: IUser | null;
+  token: string | null;
+  handleAuthenticate: (params: FormLoginParams) => Promise<void>;
+  handleRegister: (params: FormRegisterParams) => Promise<void>;
+  handleLogout: () => void;
+};
+
+export const AuthContext = createContext<AuthContextType>(
+  {} as AuthContextType
+);
+
+export const AuthContextProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [user, setUser] = useState<IUser | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+
+  const handleAuthenticate = async (userData: FormLoginParams) => {
+    const { token, user } = await authService.authenticate(userData);
+    console.log(token, user);
+
+    setUser(user);
+    setToken(token);
+  };
+
+  const handleRegister = async (formData: FormRegisterParams) => {
+    const { token, user } = await authService.registerUser(formData);
+    setUser(user);
+    setToken(token);
+  };
+
+  const handleLogout = () => {};
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        token,
+        handleAuthenticate,
+        handleRegister,
+        handleLogout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+
+  return context;
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,24 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { PublicRoutes } from "./PublicRoutes";
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 import { PrivateRoutes } from "./PrivateRoutes";
+import { PublicRoutes } from "./PublicRoutes";
+import { SystemBars } from "react-native-edge-to-edge";
+import { useAuthContext } from "@/context/auth.context";
 
 const NavigationRoutes = () => {
-  const [user, setUser] = useState();
+  const { token, user } = useAuthContext();
 
   const Routes = useCallback(() => {
-    if (!user) {
+    if (!user || !token) {
       return <PublicRoutes />;
     } else {
       return <PrivateRoutes />;
     }
-  }, [user]);
+  }, [user, token]);
 
   return (
     <NavigationContainer>
+      <SystemBars style="light" />
       <Routes />
     </NavigationContainer>
   );
