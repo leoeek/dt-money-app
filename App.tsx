ec12cf216c3876db4dbc2eb89fa974b96c56d237
leoprocopio <leonardolp@gmail.com>
2025-08-17 12:53:28 -0300

    feat: add hook form

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import "@/styles/global.css";
 import NavigationRoutes from "@/routes";
+import "./src/styles/global.css";
 
 export default function App() {
   return <NavigationRoutes />;
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,9 +1,23 @@
-import { Text, View } from "react-native";
+import { DismissKeyboardView } from "@/components/DismissKeyboard";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { View } from "react-native";
+import { LoginForm } from "./LoginForm";
+
+export type PublicStackParamsList = {
+  Login: undefined;
+  Register: undefined;
+};
 
 export const Login = () => {
+  const navigation =
+    useNavigation<StackNavigationProp<PublicStackParamsList>>();
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Tela de login!</Text>
-    </View>
+    <DismissKeyboardView>
+      <View className="flex-1 w-[82%] self-center">
+        <LoginForm />
+      </View>
+    </DismissKeyboardView>
   );
 };
