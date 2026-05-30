53fc3612999a5af4da260c424acc737acc68211b
leoprocopio <leonardolp@gmail.com>
2026-05-30 13:39:27 -0300

    feat˜

--- App.tsx
+++ App.tsx
@@ -3,14 +3,20 @@ import "./src/styles/global.css";
 import { AuthContextProvider } from "@/context/auth.context";
 import { SnackbarContextProvider } from "@/context/snackbar.context";
 import { Snackbar } from "@/components/Snackbar";
+import { BottomSheetProvider } from "@/context/bottomsheet.context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <SnackbarContextProvider>
-      <AuthContextProvider>
-        <NavigationRoutes />
-        <Snackbar />
-      </AuthContextProvider>
-    </SnackbarContextProvider>
+    <GestureHandlerRootView className="flex-1">
+      <SnackbarContextProvider>
+        <AuthContextProvider>
+          <BottomSheetProvider>
+            <NavigationRoutes />
+            <Snackbar />
+          </BottomSheetProvider>
+        </AuthContextProvider>
+      </SnackbarContextProvider>
+    </GestureHandlerRootView>
   );
 }
